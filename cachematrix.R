991a53be951a33e38cb0fccc889fe86318c42ca4
arayzha <43499528+arayzha@users.noreply.github.com>
2018-09-22 23:14:37 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,18 +5,22 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         #make initial value for inverse (i)
-		i <- NULL
-		#set the value of x
+	i <- NULL
+	
+	#set the value of x
         set <- function(y) {
                 x <<- y
                 i <<- NULL
         }
         get <- function() x
-		#set the inverse value
+	
+	#set the inverse value
         setinv <- function(inverse) i <<- inverse
-        #get the inverse value
-		getinv <- function() i
-		#listing the result for return
+        
+	#get the inverse value
+	getinv <- function() i
+	
+	#listing the result for return
         list(set = set, get = get,
              setinv = setinv ,
              getinv = getinv )
@@ -26,19 +30,22 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-		#get the value of i (if any)
+	#get the value of i (if any)
         i <- x$getinv()
-		#if the value of i already exists, then get it from cached data and skip calculation
+	
+	#if the value of i already exists, then get it from cached data and skip calculation
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
         }
-		#if not exists, calculate inverse
+	
+	#if not exists, calculate inverse
         data <- x$get()
         i <- solve(data, ...)
-        #make cache
-		x$setinv (i)
-		## Return a matrix that is the inverse of 'x'
+        
+	#make cache
+	x$setinv (i)
+	## Return a matrix that is the inverse of 'x'
         i
         
 }
